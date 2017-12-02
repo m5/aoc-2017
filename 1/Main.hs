8ffa9b47b46baf2db3ac544698637c3833402847
m5 <micah@qwerjk.com>
2017-12-01 22:04:58 -0500

    Day 1

---
+++ 1/Main.hs
@@ -0,0 +1,25 @@
+import System.IO
+import Control.Monad
+import Data.Char (digitToInt)
+import qualified Data.Text as T
+
+rotate :: Int -> [a] -> [a]
+rotate n xs = take (length xs) . drop n . cycle $ xs
+
+areTwins :: (Int, Int) -> Bool
+areTwins (a, b) = a == b
+
+takeFirst :: (Int, Int) -> Int
+takeFirst (a, b) = a
+
+main :: IO ()
+main = do
+    handle <- openFile "input" ReadMode
+    contents <- hGetContents handle
+    let stripped = T.unpack . T.strip . T.pack $ contents
+        digits = map digitToInt stripped
+        zipped = zip digits (rotate 1 digits)
+        digsum = sum . map takeFirst . filter areTwins $ zipped
+
+    print digsum
+    hClose handle
